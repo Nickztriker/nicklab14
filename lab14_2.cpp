9cf94d7fc4cd101a58bbbc798310bb063415fe02
Nickztriker <nick@coder.com>
2024-01-05 17:03:26 +0700

    Completed

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -19,3 +19,40 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+
+void showMatrix(const bool a[][N]){
+	for(int i=0; i<N; i++){
+		for(int j=0; j<N; j++){
+			cout << a[i][j];
+			cout << " ";
+		}
+		cout << endl;
+	}
+}
+
+void inputMatrix(double b[][N]){
+	for(int i=0; i<N; i++){
+		cout << "Row " << i+1 << ": ";
+		for(int j=0; j<N; j++){
+			cin >> b[i][j];
+		}
+	}
+}
+
+void findLocalMax(const double a[][N], bool b[][N]){
+	for(int i=0; i<N; i++){
+		b[i][0] = 0;
+		b[0][i] = 0;
+		b[i][N-1] = 0;
+		b[N-1][i] = 0; 
+	}
+	for(int i=1; i<N-1; i++){
+		for(int j=1; j<N-1; j++){
+			if(a[i][j] >= a[i-1][j] && a[i][j] >= a[i][j+1] && a[i][j] >= a[i+1][j] && a[i][j] >= a[i][j-1]){
+				b[i][j] = true;
+			}else{
+				b[i][j] = false;
+			}
+		}
+	}
+}
